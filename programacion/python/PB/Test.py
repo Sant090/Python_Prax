008fb607d68713e33ff18c09885088a48ff382f5
Sant090 <santiagoaroca090@gmail.com>
2024-07-16 20:05:26 -0500

    up

--- programacion/python/PB/Test.py
+++ programacion/python/PB/Test.py
@@ -1,3 +1,5 @@
+#calculadora basica
+"""
 A=int(input("elije un numero " ))
 B=int(input("elije otro numero " ))
 resultado=0
@@ -26,6 +28,41 @@ while 0==0:
         print(f"Al dividir {A} y {B} da como resultado: {resultado}")
         break
     else:
-        print("La operacion que elegiste no es valida.")
-
+        print("La operacion que elegiste no es valida.")"""
 #salu3 internautas
+
+#determinar mayor y menor entre 3 numeros
+"""
+a=int(input("Ingrese el primer numero "))
+b=int(input("Ingrese el segundo numero "))
+c=int(input("Ingrese el tercer numero "))
+
+if a>=b and a>=c:
+     print(f"{a} es mayor")
+elif b>=c and b>=a:
+    print(f"{b} es mayor")
+else:
+    print(f"{c} es mayor")
+
+if a<=b and a<=c:
+     print(f"{a} es mayor")
+elif b<=c and b<=a:
+    print(f"{b} es mayor")
+else:
+    print(f"{c} es mayor")
+"""
+
+#multiplos de un numero hasta el 10
+
+a=int(input("ingrese un numero "))
+x=1
+print(f"la tabla de multiplicar del 1 al 10 de {a} es:")
+while x<=10:
+    print(f" {x}x{a} = ({x*a})")
+    x+=1
+
+
+
+
+
+
